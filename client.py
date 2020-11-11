0529a4de3514b84529afef2a0b50abd40704e6d3
Jacob Tye <jacob@eminenceaus.com>
2020-11-11 19:40:50 +1100

    Diffie Helman Key Exhange

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import math
 import random
 from hashlib import sha256
 
+
 def fastModular(b, e, n):
     if (n == 1):
         return 0
@@ -16,18 +17,21 @@ def fastModular(b, e, n):
         b = (b*b) % n
     return rs
 
+
 def rsaEncrypt(message, key):
     encryptedMessage = fastModular(message, key[1], key[0])
     return encryptedMessage
 
+
 def rsaVerify(message, key, signature):
-    messageInBytes = bytes(message)
+    messageInBytes = message.to_bytes(1000, 'little')
     hash = int.from_bytes(sha256(messageInBytes).digest(), byteorder='big')
     hashFromSignature = pow(signature, key[1], key[0])
     if(hash == hashFromSignature):
         return True
     else:
         return False
+
 
 HOST = 'localhost'    # The remote host
 PORT = 50007          # The same port as used by the server
@@ -52,20 +56,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         print("Setup Phase COMPLETE")
         print("-----------------\n")
 
-
         print("-----------------")
         print("Handshake Phase STARTED")
         print("-----------------\n")
-        
+
         # --------------------------------------
-        # CLIENT SENDS ID_C TO SERVER AND ENCRYPTS IT WITH PUBLIC KEY RETRIEVED FROM THE SETUP PHASE - SERVER WILL DECRYPT WITH PUBLIC KEY 
+        # CLIENT SENDS ID_C TO SERVER AND ENCRYPTS IT WITH PUBLIC KEY RETRIEVED FROM THE SETUP PHASE - SERVER WILL DECRYPT WITH PUBLIC KEY
         # --------------------------------------
 
         # Generate random client ID_c
         client_IDc = random.randrange(999999999)
-        s.sendall(rsaEncrypt(client_IDc, publicKey).to_bytes(1024, byteorder='little'))
+        s.sendall(rsaEncrypt(client_IDc, publicKey).to_bytes(
+            1024, byteorder='little'))
         print('Client to Server: Client IDc (Before RSA Encryption)=', client_IDc)
-
 
         # --------------------------------------
         # CLIENT RECEIVES SIGNED ID_C AND SESSION ID FROM SERVER AND VERIFIES SIGNATURES OF BOTH MESSAGES WITH PUBLIC KEY RETRIEVED FROM THE SETUP PHASE
@@ -81,7 +84,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print('Server to Client: Server IDc:', server_IDc)
         else:
             # If verification fails deny server IDc and throw an error
-            raise ValueError('The signature attached to "Server IDc" could not be verified\n')
+            raise ValueError(
+                'The signature attached to "Server IDc" could not be verified\n')
 
         # Receive server IDc from the server followed by the server_IDc signature
         session_ID = int.from_bytes(s.recv(1024), byteorder="little")
@@ -93,7 +97,41 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print('Server to Client: Session ID:', session_ID)
         else:
             # If verification fails deny server IDc and throw an error
-            raise ValueError('The signature attached to "Session ID" could not be verified\n')
+            raise ValueError(
+                'The signature attached to "Session ID" could not be verified\n')
+
+        # --------------------------------------
+        # DIFFIE HELMAN KEY EXCHANGE
+        # --------------------------------------
+
+        print('\nINITIATED DIFFIE HELMAN KEY EXCHANGE\n')        
+
+        p = 178011905478542266528237562450159990145232156369120674273274450314442865788737020770612695252123463079567156784778466449970650770920727857050009668388144034129745221171818506047231150039301079959358067395348717066319802262019714966524135060945913707594956514672855690606794135837542707371727429551343320695239
+        g = 174068207532402095185811980123523436538604490794561350978495831040599953488455823147851597408940950725307797094915759492368300574252438761037084473467180148876118103083043754985190983472601550494691329488083395492313850000361646482644608492304078721818959999056496097769368017749273708962006689187956744210730
+        clientSecret = random.randrange(99)
+
+        # Compute A to send to the Server
+        A = fastModular(g, clientSecret, p)
+
+        # Send encrypted computation to server
+        print('Client to Server: A Computation (Before RSA Encryption)=', A)
+        s.sendall(rsaEncrypt(A, publicKey).to_bytes(1024, byteorder='little'))
+
+        # Receive server IDc from the server followed by the server_IDc signature
+        serverB = int.from_bytes(s.recv(1024), byteorder="little")
+        serverB_sign = int.from_bytes(s.recv(1024), byteorder="little")
+
+        # Verify the signature attached to the message from server
+        if(rsaVerify(serverB, publicKey, serverB_sign) == True):
+            # If verification passes accept the server IDc
+            print('\nServer to Client: Server B:', serverB)
+        else:
+            # If verification fails deny server IDc and throw an error
+            raise ValueError(
+                'The signature attached to "Server B" could not be verified\n')
+
+        clientSharedSecret = fastModular(serverB, clientSecret, p)
+        print('\nClient Shared Secret:', clientSharedSecret)
 
         print("\n-----------------")
         print("Handshake Phase COMPLETE")
--- server.py
+++ server.py
@@ -35,7 +35,7 @@ def generateRsaKeyPair():
         if math.gcd(e, phiN) == 1:
             break
 
-    # The inverse of the public key and phi N = the private key 
+    # The inverse of the public key and phi N = the private key
     d = number.inverse(e, (p - 1) * (q - 1))
 
     # Set keys
@@ -50,8 +50,9 @@ def rsaDecrypt(message, key):
     decryptedMessage = fastModular(message, key[1], key[0])
     return decryptedMessage
 
+
 def rsaSignature(message, key):
-    messageInBytes = bytes(message)
+    messageInBytes = message.to_bytes(1000, 'little')
     hash = int.from_bytes(sha256(messageInBytes).digest(), byteorder='big')
     signature = fastModular(hash, key[1], key[0])
     return signature
@@ -81,16 +82,41 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 ((serverKeys[0][1]).to_bytes(2048, byteorder='little')))
 
             # Handshake phase - receive client id
-            clientIDc = rsaDecrypt(int.from_bytes(conn.recv(1024), byteorder="little"), serverKeys[1])
+            clientIDc = rsaDecrypt(int.from_bytes(
+                conn.recv(1024), byteorder="little"), serverKeys[1])
             if not clientIDc:
                 break
 
             # Sending server ID to the client, signing the message and sending the signature for client to verify
             server_IDc = random.randrange(999999999)
             conn.sendall(server_IDc.to_bytes(1024, byteorder='little'))
-            conn.sendall(rsaSignature(server_IDc, serverKeys[1]).to_bytes(1024, byteorder='little'))
+            conn.sendall(rsaSignature(server_IDc, serverKeys[1]).to_bytes(
+                1024, byteorder='little'))
 
             # Sending session ID to the client, signing the message and sending the signature for client to verify
             session_ID = random.randrange(999999999)
             conn.sendall(session_ID.to_bytes(1024, byteorder='little'))
-            conn.sendall(rsaSignature(session_ID, serverKeys[1]).to_bytes(1024, byteorder='little'))
+            conn.sendall(rsaSignature(session_ID, serverKeys[1]).to_bytes(
+                1024, byteorder='little'))
+
+            # Starting Diffie Helman Key Exchange
+            # Receives Client computation and decrypts it
+            clientA = rsaDecrypt(int.from_bytes(
+                conn.recv(1024), byteorder="little"), serverKeys[1])
+            if not clientA:
+                break
+
+            p = 178011905478542266528237562450159990145232156369120674273274450314442865788737020770612695252123463079567156784778466449970650770920727857050009668388144034129745221171818506047231150039301079959358067395348717066319802262019714966524135060945913707594956514672855690606794135837542707371727429551343320695239
+            g = 174068207532402095185811980123523436538604490794561350978495831040599953488455823147851597408940950725307797094915759492368300574252438761037084473467180148876118103083043754985190983472601550494691329488083395492313850000361646482644608492304078721818959999056496097769368017749273708962006689187956744210730
+            serverSecret = random.randrange(99)
+
+            # Compute B to send to client
+            B = fastModular(g, serverSecret, p)
+
+            serverSharedSecret = fastModular(clientA, serverSecret, p)
+            print('\nServer Shared Secret: ', serverSharedSecret)
+
+            # Send computation w/ signature
+            conn.sendall(B.to_bytes(1024, byteorder='little'))
+            conn.sendall(rsaSignature(B, serverKeys[1]).to_bytes(
+                1024, byteorder='little'))
